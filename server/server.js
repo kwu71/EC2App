11ac722a4ce2f2a8a86b60914f692c8d6c7667d4
Kevin Wu <kevinwu@Macbook-Pro-Kevin.local>
2023-09-29 15:16:27 -0400

    get well arthur

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const app = express();
 const port = 5001
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Hi, Arthur! I hope you feel well. 😁')
   })
   
 app.listen(port, () => {
